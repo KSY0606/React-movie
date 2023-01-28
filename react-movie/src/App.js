7bbc0212cdd45ce6c308affd28afda2d1c0e1d30
KSY0606 <tkddbs9506@naver.com>
2023-01-28 12:30:41 +0900

    react movie

--- react-movie/src/App.js
+++ react-movie/src/App.js
@@ -1,43 +1,25 @@
-import { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+import "./App.css";
 
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  const getMovies = async() => {
-    const response = await fetch(
-      'https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=yaer'
-    );
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-  
   return (
-    <div>
-      {loading ? (
-      <h1>Loading..</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-          <div key={movie.id}>
-            <img src={movie.medium_cover_image}/>
-            <h2>{movie.title}</h2>
-            <p>{movie.summary}</p>
-            <ul>
-              {movie.genres.map(g => <li key={g}>{g}</li>)}
-            </ul>
-          </div>
-      ))}
-        </div>
-      )}
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/movie/:id" element={<Detail/>}>
+        </Route>
+
+        <Route path="/" element={<Home/>}>
+        </Route>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
